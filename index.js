2c17ad0847690e27a3f80365c12209ed4265243e
Joseph <josephshonko99@gmail.com>
2024-03-21 08:55:15 +0300

    commiting arrow function

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = function(a,b) {return a/b};
+let a=2000;
+let b=100;
+const parameter = [2,5]
+function add(a,b){
+    return a + b;
+
+}
+const square = () => {
+    let x = 2
+    return x **2;
+}
